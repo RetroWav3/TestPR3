08c119c741079b2a5e3f83e653db25d58ee83021
Retro <regetnix@yandex.ru>
2022-10-11 15:15:23 +0300

    setup timer test

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-'''ochen_horosho'''
+#Будильник
+
+
+def setup_timer():
+    pass
+
+def setup_timer_test():
+
+    print('Установите время пробуждения: ')
+
+    print('Часы: ')
+    hours = 12
+    print(hours)
+
+    print('Минуты: ')
+    minutes = 30
+    print(minutes)
+
+    fulltime = str(hours) + str(minutes)
+
+    return fulltime
+
+print(setup_timer_test())
